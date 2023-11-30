2d0a35bd37f5da828b75cded6e2ac2c02cc98745
mpatnode1 <patnode@colum.edu>
2023-11-30 15:12:29 -0600

    Golf Club + Packages
    - Added Swinging Golf Club
    - Added Packages for golf club, nature elements, and golf ball
    -Added file for documentation/credits

---
+++ Assets/CreditsReadMe.cs
@@ -0,0 +1,28 @@
+/*
+
+Itch.io copy text
+It's time to face the scariest thing imaginable,
+golf.
+
+Alright, sure, you might not think golf is all too intense or life threatening, (unless you spend a little TOO long thinking about how there's more land dedicated to golf courses than there is the state of Delaware) but 
+what if you were the golf ball?
+How would you like it if I hit you with a golf club? Now you can find out!
+
+
+VFX Confetti on Goal Trigger:
+Hyper Casual FX by Lana Studio
+https://assetstore.unity.com/packages/vfx/particles/hyper-casual-fx-200333#description
+
+
+Golf Club Asset:
+Used Golf Club-Free by Alvarolopz98
+https://assetstore.unity.com/packages/3d/props/used-golf-club-free-201581
+
+
+Golf Ball Material:
+Free Sports Kit by Sports-Actions
+https://assetstore.unity.com/packages/3d/characters/free-sports-kit-239377
+
+
+
+*/
