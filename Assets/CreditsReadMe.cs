f29cf42db5edafc9062bb40ed33e73b44e406092
mpatnode1 <149105967+mpatnode1@users.noreply.github.com>
2023-12-13 21:09:52 -0600

    finished compiling scripts
    to convert to webgl build

--- Assets/CreditsReadMe.cs
+++ Assets/CreditsReadMe.cs
@@ -13,16 +13,30 @@ VFX Confetti on Goal Trigger:
 Hyper Casual FX by Lana Studio
 https://assetstore.unity.com/packages/vfx/particles/hyper-casual-fx-200333#description
 
+VFX Constant Leaf Falling:
+Farming Game FX by Pang.Noidee
+https://assetstore.unity.com/packages/vfx/particles/environment/farming-game-fx-mission-236550
+
 
 Golf Club Asset:
 Used Golf Club-Free by Alvarolopz98
 https://assetstore.unity.com/packages/3d/props/used-golf-club-free-201581
-
 
 Golf Ball Material:
 Free Sports Kit by Sports-Actions
 https://assetstore.unity.com/packages/3d/characters/free-sports-kit-239377
 
 
+Audio Triggered Effects (Win, Lose, Death):
+True 8-bit Sound Effect Collection - Lite version by AudioJackGames
+https://assetstore.unity.com/packages/audio/sound-fx/true-8-bit-sound-effect-collection-lite-version-264063
+
+In-Game Background Music:
+Let's Go! Action Soundtrack by TFSui
+https://assetstore.unity.com/packages/audio/music/let-s-go-action-soundtrack-56457
+
+Menu Music:
+Music - Fun & Games by SD Sound Tracks
+https://assetstore.unity.com/packages/audio/music/music-fun-games-157280
 
 */
